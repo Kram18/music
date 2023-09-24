5dd6e8c83272d9cf02609986c779abf07b877e6a
Mjoseph18 <112936142+Kram18@users.noreply.github.com>
2023-09-24 22:16:01 +0800

    main

---
+++ app/Controllers/MusicController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class MusicController extends BaseController
+{
+
+    public function musicsick()
+    {
+       //eturn view('main', $data);
+       echo "hello world";
+    }
+
+
+
+
+    public function index()
+    {
+        //
+        
+    }
+}
